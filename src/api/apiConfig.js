3270e90699dddce11bd51892f6813e1bd10bfcd3
Marcin Czajka <czajka.marcin@protonmail.com>
2021-06-18 09:40:26 +0200

    Decided to publish api endpoints after all. I ll try to make repo private for now

---
+++ src/api/apiConfig.js
@@ -0,0 +1,4 @@
+export const basePath = 'https://thebetter.bsgroup.eu';
+export const loginPath = basePath + '/Authorization/SignIn';
+export const mediaListPath = basePath + '/Media/GetMediaList';
+export const mediaPath = basePath + '/Media/GetMediaPlayInfo';
